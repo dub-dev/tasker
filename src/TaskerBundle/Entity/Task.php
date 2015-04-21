481c3cda0fc2d04fc699c88846079b390143775b
Dubouchet <thibaud.dubouchet@epitech.eu>
2015-04-21 13:36:36 +0200

    Creating Entity for Categories & Tasks

---
+++ src/TaskerBundle/Entity/Task.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace TaskerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Task
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="cat_id", type="integer")
+     */
+    private $catId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="active", type="integer")
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set catId
+     *
+     * @param integer $catId
+     * @return Task
+     */
+    public function setCatId($catId)
+    {
+        $this->catId = $catId;
+
+        return $this;
+    }
+
+    /**
+     * Get catId
+     *
+     * @return integer 
+     */
+    public function getCatId()
+    {
+        return $this->catId;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Task
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Task
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set active
+     *
+     * @param integer $active
+     * @return Task
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return integer 
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
