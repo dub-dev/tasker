a9944138673d4eb8bfbaee5d619dda3a09016f62
Dubouchet <thibaud.dubouchet@epitech.eu>
2015-04-21 15:33:09 +0200

    Dashboard now ok, improving UX

--- src/TaskerBundle/Controller/DefaultController.php
+++ src/TaskerBundle/Controller/DefaultController.php
@@ -13,6 +13,8 @@ class DefaultController extends Controller
 
     public function dashboardAction()
     {
-        return $this->render('TaskerBundle:Default:dashboard.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $boards = $em->getRepository('TaskerBundle:Board')->findByUser($this->getUser());
+        return $this->render('TaskerBundle:Default:dashboard.html.twig', array('boards' => $boards));
     }
 }
